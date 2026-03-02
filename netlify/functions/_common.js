b7a9a439b7e9a4217a713ec0fa80ae6fed95be68
Wecost305 <wecostuno@gmail.com>
2026-03-01 23:26:28 -0600

    Delete netlify/functions directory

--- netlify/functions/_common.js
+++
@@ -1,7 +0,0 @@
-export const json = (status, obj) => new Response(JSON.stringify(obj), {
-  status,
-  headers: {
-    'content-type': 'application/json; charset=utf-8',
-    'cache-control': 'no-store'
-  }
-});
